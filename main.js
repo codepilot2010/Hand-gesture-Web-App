59707a890557aff8d81235c833933117a9fd1d9e
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-03-21 19:36:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function take_snapshot()
 
 console.log('ml5 version: ',ml5.version);
 
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JTYDm0Fea/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ginaTJiOr/model.json',modelLoaded);
 
 function modelLoaded()
 {
